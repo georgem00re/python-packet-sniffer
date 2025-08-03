794c51d5c47c4e40788bcb995d39c135e248437a
george <george.moore@cyted.ai>
2025-08-03 12:55:56 +0100

    update main.py script to log packets on en0 interface

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
 
-from scapy.all import *
-packet = IP(dst="8.8.8.8")/ICMP()
-packet.show()
+from scapy.all import sniff, get_if_list
+import sys
+
+INTERFACE = "en0"
+
+def log_packet(packet):
+    print(packet.summary())
+
+def get_all_network_interfaces():
+    get_if_list()
+
+def main():
+    if INTERFACE not in get_if_list(): sys.exit(1)
+    sniff(iface=INTERFACE, prn=log_packet, store=False)
+
+if __name__ == "__main__":
+    main()
